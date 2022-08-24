3cc8989e833b8d9e439d2213082d0b835c11e0ea
elaniacs <carensousaesilva@gmail.com>
2022-08-24 15:45:45 -0300

    implementing multiple choices and alerting

--- quizzler/quizzler/Controller/ViewController.swift
+++ quizzler/quizzler/Controller/ViewController.swift
@@ -11,21 +11,25 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var choice1: UIButton!
+    @IBOutlet weak var choice2: UIButton!
+    @IBOutlet weak var choice3: UIButton!
     @IBOutlet weak var scoreLabel: UILabel!
     
     var quizBrain = QuizBrain()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        quizBrain.uiView = self
+ 
         updateUI()
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
-        let userAnswer = sender.currentTitle! // True, False
+        let userAnswer = sender.currentTitle!
         let userGotItRight = quizBrain.checkAnswer(userAnswer)
         
         if userGotItRight {
@@ -43,7 +47,21 @@ class ViewController: UIViewController {
         questionLabel.text = quizBrain.getQuestionText()
         progressBar.progress = quizBrain.getProgress()
         scoreLabel.text = "Score: \(quizBrain.getScore())"
-        trueButton.backgroundColor = UIColor.clear
-        falseButton.backgroundColor = UIColor.clear
+        choice1.setTitle(quizBrain.getOptionsAnswer()[0], for: .normal)
+        choice2.setTitle(quizBrain.getOptionsAnswer()[1], for: .normal)
+        choice3.setTitle(quizBrain.getOptionsAnswer()[2], for: .normal)
+        choice1.backgroundColor = UIColor.clear
+        choice2.backgroundColor = UIColor.clear
+        choice3.backgroundColor = UIColor.clear
+    }
+    
+    func showAlert() {
+        
+        let alert = UIAlertController(title: "Done!", message: scoreLabel.text!, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: {_ in self.view.alpha = 1.0}))
+        
+        self.view.alpha = 0.2
+        
+        self.present(alert, animated: true, completion: nil )
     }
 }
